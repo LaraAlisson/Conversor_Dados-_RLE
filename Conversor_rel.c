9139660c38ff5964e1fe92e23231e3c77fb7d4cf
Alisson Ramao Lara <alisson.lara@videplast.com.br>
2025-11-13 13:58:03 -0300

    Inicio de projeto de conversor de dados RLE

---
+++ Conversor_rel.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int main(){
+
+   FILE *fp;
+    long file_size;
+    char *p_str;
+    
+    fp = fopen("Arquivo de texto.txt", "r");
+    if (fp == NULL) {
+        perror("ERROR: opening file");
+        return 1;
+    }
+
+    // 1. Obter tamanho
+    fseek(fp, 0, SEEK_END);
+    file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    // 2. Alocar memória
+    p_str = (char *)malloc(file_size + 1); 
+    if (p_str == NULL) {
+        fclose(fp);
+        perror("ERROR: memory allocation failed");
+        return 1;
+    }
+
+    // 3. Ler o arquivo
+    fread(p_str, sizeof(char), file_size, fp);
+
+    // 4. Finalizar string
+    p_str[file_size] = '\0';
+
+    fclose(fp);
+
+    printf("Tamanho lido: %ld bytes\n", file_size);
+    printf("Conteúdo: \n%s\n", p_str);
+
+    // 5. Liberar memória
+    free(p_str);
+
+    return 0;
+}
